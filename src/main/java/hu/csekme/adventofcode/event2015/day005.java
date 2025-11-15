a730590e3a2e932a0ab903f30f3e72e1cadf183b
Csekme Krisztián <csekme.krisztian@outlook.com>
2025-11-15 17:03:45 +0100

    Add day005 implementation to check for additional "nice" line criteria

--- src/main/java/hu/csekme/adventofcode/event2015/day005.java
+++ src/main/java/hu/csekme/adventofcode/event2015/day005.java
@@ -28,6 +28,35 @@ public class day005 implements CommandLineRunner {
         return false;
     }
 
+    boolean hasDuplicatesOfTwo(String line) {
+        char[] chars = line.toCharArray();
+        for (int i = 1; i < chars.length; i++) {
+            char first = chars[i - 1];
+            char second = chars[i];
+            for (int j = 1; j < chars.length; j++) {
+                if (first == chars[j - 1] && chars[j] == second && (j<i-1 || j>i+1)) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    boolean hasDuplicatesWithBrake(String line) {
+        char[] chars = line.toCharArray();
+        for (int i = 2; i < chars.length; i++) {
+            char first = chars[i - 2];
+            char second = chars[i];
+            if (first == second) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
     int countOfM(String line) {
         int count = 0;
         for (char c : line.toCharArray()) {
@@ -45,8 +74,16 @@ public class day005 implements CommandLineRunner {
             List<String> forbiddens = Arrays.asList("ab", "cd", "pq", "xy");
             int nice = 0;
             for (String line : linesOption.get()) {
-                log.info(line);
+
                 if (hasDuplicates(line) && countOfM(line) > 2 && forbiddens.stream().noneMatch(line::contains)) {
+                    nice++;
+                }
+            }
+            log.info(nice + " nice lines");
+            nice = 0;
+            for (String line : linesOption.get()) {
+
+                if (hasDuplicatesOfTwo(line) && hasDuplicatesWithBrake(line)) {
                     nice++;
                 }
             }
